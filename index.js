d3827f07add0d37943f1a064d452f8d2043d232b
jarvis <sahilbarak737@gmail.com>
2023-04-23 17:29:25 +0530

    remove email function

--- index.js
+++ index.js
@@ -131,37 +131,6 @@ const onFileUploadSuccess = (res) => {
     fileURL.value = url;
 };
 
-emailForm.addEventListener("submit", (e) => {
-    e.preventDefault(); // stop submission
-
-    // disable the button
-    emailForm[2].setAttribute("disabled", "true");
-    emailForm[2].innerText = "Sending";
-
-    const url = fileURL.value;
-
-    const formData = {
-        uuid: url.split("/").splice(-1, 1)[0],
-        emailTo: emailForm.elements["to-email"].value,
-        emailFrom: emailForm.elements["from-email"].value,
-    };
-    console.log(formData);
-    fetch(emailURL, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-    })
-        .then((res) => res.json())
-        .then((data) => {
-            if (data.success) {
-                showToast("Email Sent");
-                sharingContainer.style.display = "none"; // hide the box
-            }
-        });
-});
-
 let toastTimer;
 // the toast function
 const showToast = (msg) => {
